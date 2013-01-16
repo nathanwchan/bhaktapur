cc47f8d768b00212da0f8cadee8ee990d003b48d
nathanwchan <nchan87@gmail.com>
2013-01-17 02:28:12 +0800

    add edit project functionality

--- client/bhaktapur.js
+++ client/bhaktapur.js
@@ -32,6 +32,21 @@ Template.projects.projectsLoaded = function () {
 	return Session.get('projectsLoaded');
 };
 
+Template.project.isLogged = function () {
+  	return (Session.get("accessToken") || null) !== null;
+};
+
+Template.project.events({
+  "click #sign-in": function () {
+    showLoginPopup();
+  },
+  "click #edit-project": function () {
+  	$('#projectId_edit').attr("value", this._id)
+  	$('#name_edit').attr("value", this.name);
+	$('#description_edit').attr("value", this.description);
+  }
+});
+
 Template.users.users = function () {
 	return Users.find({}, {sort: {name: -1}});
 };
@@ -40,8 +55,8 @@ Template.users.usersLoaded = function () {
 	return Session.get('usersLoaded');
 };
 
-Template.modals.events = {
-	'click input.add': function () {
+Template.addProjectModal.events = {
+	'click input.submit': function () {
 		$('#name_error').prop("innerHTML", "");
 		$('#submitButton').attr("disabled", true);
 		$('#submitSpinner').css("visibility", "visible")
@@ -60,6 +75,32 @@ Template.modals.events = {
 					$('#submitSpinner').css("visibility", "hidden");
 					$('input#name').attr("value", "");
 					$('textarea#description').attr("value", "");
+				}
+			});
+	}
+};
+
+Template.editProjectModal.events = {
+	'click input.submit': function () {
+		$('#name_error_edit').prop("innerHTML", "");
+		$('#submitButton_edit').attr("disabled", true);
+		$('#submitSpinner_edit').css("visibility", "visible")
+		Meteor.call('editProject',
+			$('#projectId_edit').val(),
+			$('#name_edit').val(),
+			$('#description_edit').val(),
+			function (error, result) {
+				if (!result) {
+					$('#name_error_edit').prop("innerHTML", "Give the project a name!");
+					$('#submitButton_edit').removeAttr("disabled");
+					$('#submitSpinner_edit').css("visibility", "hidden")
+				}
+				else {
+					$('#editproject').modal('hide');
+					$('#submitButton_edit').removeAttr("disabled");
+					$('#submitSpinner_edit').css("visibility", "hidden");
+					$('#name_edit').attr("value", "");
+					$('#description_edit').attr("value", "");
 				}
 			});
 	}
--- server/methods.js
+++ server/methods.js
@@ -1,6 +1,5 @@
 Meteor.methods({
   addProject: function (name, description) {
-    // .. do stuff ..
     if (!name)
       throw new Meteor.Error(404, "Must provide project name!");
     var date = (new Date()).getTime();
@@ -9,6 +8,21 @@ Meteor.methods({
       description: description,
       date: date
     });
+    return true;
+  },
+
+  editProject: function (id, name, description) {
+    if (!name)
+      throw new Meteor.Error(404, "Must provide project name!");
+    var date = (new Date()).getTime();
+    Projects.update(
+      {_id: id},
+      {
+        name: name,
+        description: description,
+        date: date
+      }
+    );
     return true;
   },
 
